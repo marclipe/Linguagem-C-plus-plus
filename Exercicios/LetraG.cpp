37e483e7bb022d6c851e8783d0d424e3b839e197
MarcosF19 <marcosfelipek10@gmail.com>
2021-04-08 22:58:44 -0300

    terminando

--- Exercicios/LetraG.cpp
+++ Exercicios/LetraG.cpp
@@ -1,10 +1,32 @@
 #include <iostream> 
+#include <cstdlib>
+#include <string.h>
 using namespace std;
 
 int main()
 { 
     setlocale(LC_ALL, "Portuguese");
 
+    int tecla,H; 
     Menu: 
-    cout << ""; 
+    system("clear"); 
+    cout << "\n1 Executar..."; 
+    cout << "\n2 Finalizar...";
+    cout << "\n3 Item"; 
+    cin >> tecla ; 
+
+     
+
+    switch (tecla)
+    {
+    case 1: cout << "Digite o primeiro número:  ";
+            
+            cout << "Digite o segundo número: "; 
+        break;
+    
+    default:
+        break;
+    }
+
+
 }
