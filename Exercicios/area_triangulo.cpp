24cfe02cdc6b172fc0f03d15dca6189df1d79d43
MarcosF19 <marcosfelipek10@gmail.com>
2021-05-04 21:03:48 -0300

    Programa adicionado
    Preciso realizar mudanças.

--- Exercicios/area_triangulo.cpp
+++ Exercicios/area_triangulo.cpp
@@ -1,42 +1,71 @@
 #include <iostream> //04/05/2021
 #include <cstdlib>
+#include <iomanip>
 using namespace std;
 
-double lerbase(){
-    double base; 
-    cout << "\nLer Base: "; 
-    cin >> base; 
-    return base; 
+double lerbase()
+{
+    double base;
+    cout << "\nLer Base: ";
+    cin >> base;
+    return base;
 }
 
-double leraltura(){
-    double altura; 
+double leraltura()
+{
+    double altura;
     cout << "Ler Altura: ";
     cin >> altura;
-    return altura; 
+    return altura;
 }
 
-double calcarea(double base, double altura){
-    double area; 
-    area = (base * altura)/2; 
-    return area;  
+double calcarea(double base, double altura)
+{
+    double area;
+    area = (base * altura) / 2;
+    return area;
 }
 
-void exibir (double base, double altura, double area){
+void exibir(double base, double altura, double area)
+{
     system("clear");
     cout << "\nBase.." << base;
     cout << "\nAltura.." << altura;
-    cout << "\nArea.." << area; 
-    system("clear"); 
+    cout << "\nArea.." << area;
+    system("pause 5");
 }
 
-int main (){
+int main()
+{
     double base, altura, area;
-    int tecla = 0; 
-    while( tecla!=4 ){
-        cout << "\n1 Ler.."; 
-        cout << "\n2 Calcular..."; 
-        cout << "\nExibir..."; 
-        cout << "\nSair"; 
-    }  
+    int tecla = 0;
+    while (tecla != 4)
+    {
+        system("clear");
+        cout << "\n1 Ler..";
+        cout << "\n2 Calcular...";
+        cout << "\nExibir...";
+        cout << "\nSair\nItem";
+    }
+
+    if (tecla == 1)
+    {
+        base = lerbase();
+        altura = leraltura();
+    }
+    else if (tecla == 2)
+    {
+        area = calcarea(base, altura);
+        cout << "\nCálculo bem sucedido!";
+        system("pause");
+    }
+    else if (tecla == 3)
+    {
+        exibir(base, altura, area);
+    } //fim while
+
+    cout << "\nPrograma Finalizado\n"
+         << endl;
+    system("pause");
+    return 0;
 }
