a2b6438cbe1c786ee5b74418278ecbc578567db3
MarcosF19 <marcosfelipek10@gmail.com>
2021-05-15 10:01:33 -0300

    Maior e menor
    Maior e menor pagamento

---
+++ Exercicios/alc2.cpp
@@ -0,0 +1,35 @@
+/*
+Quadro Resumo de variáveis
+ 
+1) lista, pag, Maior18, Menor18, menor18, maior18, quantmaior, quantmenor
+2) lista, pag
+3) sem cáculos
+4) Maior18, Menor18, menor18, maior18, quantmaior, quantmenor
+ 
+*/
+#include "iostream" //14/05/2021
+#include "string"
+#include "cstdlib"
+#include "math.h"
+using namespace std;
+
+double lerLista(){
+    double lista; 
+    cout << "Digite a lista de idade: "; 
+    cin >> lista; 
+    return lista; 
+}
+
+double lerPagamentos(){
+    double pag; 
+    cout << "Digite lista de pagamentos: ";
+    cin >> pag;
+    return pag; 
+}
+
+void exibir(){
+    
+}
+
+
+
