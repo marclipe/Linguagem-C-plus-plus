3e21b77006bfc47df31f36189694822acbb7cd7f
MarcosF19 <marcosfelipek10@gmail.com>
2021-05-16 20:50:51 -0300

    ok

--- Exercicios/alc1.cpp
+++ Exercicios/alc1.cpp
@@ -1,101 +1,140 @@
-/*
-Quadro Resumo de variáveis
- 
-1) sexo, salario, mediaH, mediaM, saldoH, saldoM
-2) sexo, salario
-3) media, saldo
- 
-*/
-#include "iostream" //14/05/2021
-#include "string"
-#include "cstdlib"
-#include "math.h"
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <cstdlib>
+#include <string.h>
 using namespace std;
 
-//1) sexo, salario, media, saldo
-
-string lerSexo()
-{
-    string sexo;
-    cout << "Digite o sexo: ";
-    getline(cin, sexo);
-    return sexo;
-}
-
-double lerSalario()
-{
-    double salario;
-    cout << "Digite o salário: ";
-    cin >> salario;
-    return salario;
-}
-
-
-double CalcMedia(double salario)
-{
-    double media;
-    media = salario/1;
-    return media;
-}
-
-void mostrar(double media, string sexo)
-{
-    system("cls");
-    cout << "\nMedia.." << media;
-    cout << "\nSexo" << sexo; 
-    system("pause");
-}
+int lerSe();
+double lerSa();
+double mediaM();
+double mediaH();
+void controle();
+void mostrar(double salario);
 
 int main()
 {
+    setlocale(LC_ALL, "Portuguese");
+    system("clear");
+    controle();
+}
 
-    string sexo;
-    double salario, media, saldo;
 
-    string H, F;
-
-    if (sexo == H)
+int lerSe()
+{
+    int t = 0, res = 0;
+    ;
+    while (t == 0)
     {
-        sexo = "Homem";
+        string sea;
+        string carac;
+        int cont = 0;
+        cout << "\nDigite seu Sexo[H/F]";
+        cin.ignore();
+        getline(cin, sea);
+        int veri = sea.length();
+        while (cont <= veri)
+        {
+            carac = sea.substr(cont, 1);
+            if (sea == "h" || sea == "H")
+            {
+                res = 1;
+                t = 1;
+            }
+            if (sea == "f" || sea == "F")
+            {
+                res = 0;
+                t = 1;
+            }
+            if (sea != "h" && sea != "f" && sea != "H" && sea != "F")
+            {
+                cout << "Error! Digite um sexo válido!";
+                t = 0;
+            }
+            cont++;
+        }
     }
-    else if (sexo == F)
-    {
-        sexo == "Mulher";
-    }
-    else
-    {
-        sexo == "Sexo inválido, digite novamente!";
-    }
-    cin.ignore();
-    getline(cin, sexo);
+    return res;
+}
 
-    int tecla = 0;
-    menu:
-    while (tecla != 4)
+
+double lerSa()
+{
+    double sal;
+    cout << "Digite o salario: ";
+    cin >> sal;
+    return sal;
+}
+
+double mediaM(double saldoM, int contM)
+{
+    double media;
+    media = saldoM / contM;
+    return media;
+}
+
+double mediaH(double saldoH, int contH)
+{
+    double media;
+    media = saldoH / contH;
+    return media;
+}
+
+void mostrar(double meH, double meM, int contH, int contM)
+{
+    cout << "\n** RESULTADOS **";
+    cout << "\nTotal de Homens...: " << contH;
+    cout << "\nMédia do Salario..: " << meH;
+    cout << "\nTotal de Mulheres.: " << contM;
+    cout << "\nMédia do salario..: " << meM << endl;
+    system("sleep 3");
+}
+
+void controle()
+{
+    int tecla, idade, contM = 0, contH = 0;
+    int sexo;
+    double salario, saldoM = 0, saldoH = 0;
+    double meM, meH;
+
+    //MENU 
+
+    while (true)
     {
-        system("cls");
-        cout << "\n1 Ler..";
-        cout << "\n2 Calcular...";
-        cout << "\n3 Exibir...";
-        cout << "\n4 Sair\nItem\n";
+        cout << "\n** MENU INTERATIVO **";
+        cout << "\n1 Ler valores";
+        cout << "\n2 Calcular e Exibir";
+        cout << "\n3 Desligar";
+        cout << "\nitem: ";
         cin >> tecla;
 
-        if (tecla == 1)
-    {
-        sexo = lerSexo();
-        salario = lerSalario();
-    }
-    else if (tecla == 2)
-    {
-        media = CalcMedia(salario);
-    }
-    else if (tecla == 3)
-    {
-        mostrar(saldo, sexo);
-    }
-    goto menu;
-    cout << "\nPrograma Finalizado\n";  
-    system("pause");
-    return 0;
+        switch (tecla)
+        {
+        case 1:
+            sexo = lerSe();
+            if (sexo == 1)
+            {
+                salario = lerSa();
+                saldoH += salario;
+                contH++;
+            }
+            if (sexo == 0)
+            {
+                salario = lerSa();
+                saldoM += salario;
+                contM++;
+            }
+            break;
+        case 2:
+            meH = mediaH(saldoH, contH);
+            meM = mediaM(saldoM, contM);
+            mostrar(meH, meM, contH, contM);
+            break;
+        case 3:
+            cout << "Desligando o Sistema..." << endl;
+            system("sleep 5");
+            exit(0);
+            break;
+        }
     }
 }
--- Exercicios/alc2.cpp
+++ Exercicios/alc2.cpp
@@ -1,35 +1,114 @@
-/*
-Quadro Resumo de variáveis
- 
-1) lista, pag, Maior18, Menor18, menor18, maior18, quantmaior, quantmenor
-2) lista, pag
-3) sem cáculos
-4) Maior18, Menor18, menor18, maior18, quantmaior, quantmenor
- 
-*/
-#include "iostream" //14/05/2021
-#include "string"
-#include "cstdlib"
-#include "math.h"
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <string>
 using namespace std;
 
-double lerLista(){
-    double lista; 
-    cout << "Digite a lista de idade: "; 
-    cin >> lista; 
-    return lista; 
+int lerIdade();
+double lerPag();
+void controle();
+void mostrar(int conta, double maa, double mea, int conte, double mam, double mem);
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    system("clear");
+    controle();
 }
 
-double lerPagamentos(){
-    double pag; 
-    cout << "Digite lista de pagamentos: ";
-    cin >> pag;
-    return pag; 
+int lerIdade()
+{
+    int idade;
+    cout << "\nDigite a idade: ";
+    cin >> idade;
+    idade = (idade >= 18) ? 1 : 0;
+    return idade;
 }
 
-void exibir(){
-    
+double lerPag()
+{
+    double sal;
+    cout << "Digite seu salario: ";
+    cin >> sal;
+    return sal;
 }
 
+void mostrar(int conta, double maa, double mea, int conte, double mam, double mem)
+{
+    cout << "\n** Resultado do Programa **";
+    cout << "\nTotal maior de idade: " << conta;
+    cout << "\nMaior salario.......: " << maa;
+    cout << "\nMenor salario.......: " << mea;
+    cout << "\nTotal menor de idade: " << conte;
+    cout << "\nMaior salario.......: " << mam;
+    cout << "\nMenor salario.......: " << mem;
+    cout << "\nFim...." << endl;
+    system("sleep 2");
+}
 
+void controle()
+{
+    int tecla, idade;
+    int conta = 0;
+    int conte = 0;
+    double salario, aux, maa, mea, mam, mem;
+menu:
+    cout << "\n** MENU INTERATIVO **";
+    while (true)
+    {
+        cout << "\n1 Ler valores\n2 Calcular e exibir\n3 Desligar\nitem: \n";
+        cin >> tecla;
+        switch (tecla)
+        {
+        case 1:
+            idade = lerIdade();
+            salario = lerPag();
 
+            if (idade == 1)
+            {
+                if (conta == 0)
+                {
+                    maa = salario;
+                    mea = salario;
+                }
+                if (salario > maa)
+                {
+                    maa = salario;
+                }
+                if (salario < mea)
+                {
+                    mea = salario;
+                }
+                conta++;
+            }
+
+            if (idade == 0)
+            {
+                if (conte == 0)
+                {
+                    mam = salario;
+                    mem = salario;
+                }
+                if (salario > mam)
+                {
+                    mam = salario;
+                }
+                if (salario < mem)
+                {
+                    mem = salario;
+                }
+                conte++;
+            }
+
+            break;
+        case 2:
+            mostrar(conta, maa, mea, conte, mam, mem);
+
+            break;
+        case 3:
+            cout << "\nDesligando sistema...";
+            exit(0);
+            break;
+        }
+    }
+}
